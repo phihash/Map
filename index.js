9a41775c4c7746ecd74eb36a929cf44bd8071a2b
phihash <insharp0220@gmail.com>
2025-11-01 22:10:16 +0900

    一旦勉強

--- index.js
+++ index.js
@@ -8,87 +8,99 @@ const config = {
   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.LINE_CHANNEL_SECRET,
 };
-
+const client = new line.Client(config); //今は参照されていませんが、Bot側からメッセージを送信するために必要です!
 const app = express();
 
-const client = new line.Client(config);
-
-const handleEvent = async (event) => {
-  // テキストメッセージの処理
-  if (event.type === "message" && event.message.type === "text") {
-    const receivedText = event.message.text;
-    const replyToken = event.replyToken;
-
-    if (receivedText === "トイレ") {
-      const message = {
-        type: "text",
-        text: "現在地を送信してください",
-        quickReply: {
-          items: [
-            {
-              type: "action",
-              action: {
-                type: "location",
-                label: "現在地を送信",
-              },
-            },
-          ],
-        },
-      };
-      try {
-        await client.replyMessage(replyToken, message);
-      } catch (error) {
-        console.error(error);
-      }
-      return;
-    }
-
-    const replyText = `あなたが送ったのは${receivedText}`;
-    const message = {
-      type: "text",
-      text: replyText,
-    };
-    try {
-      await client.replyMessage(replyToken, message);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  // 位置情報メッセージの処理
-  else if (event.type === "message" && event.message.type === "location") {
-    const { latitude, longitude, address } = event.message;
-    const replyToken = event.replyToken;
-
-    const message = {
-      type: "text",
-      text: `位置情報を受信しました！\n緯度: ${latitude}\n経度: ${longitude}\n住所: ${
-        address || "不明"
-      }`,
-    };
-
-    try {
-      await client.replyMessage(replyToken, message);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-};
-
 app.post("/webhook", line.middleware(config), (req, res) => {
-  const events = req.body.events;
-  Promise.all(events.map(handleEvent))
-    .then(() => {
-      res.json({ status: "ok" });
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).json({ status: "error" });
-    });
-
-  // ここで受信したイベント（メッセージなど）を処理するコードを後で追加
+  console.log("🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥");
+  console.log(req.body);
+  console.log("🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥");
+  console.log(req.body.events);
+  // ここに色々かいていきます!
+  res.json({ status: "ok" });
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}...`);
 });
+
+// const handleEvent = async (event) => {
+//   // テキストメッセージの処理
+//   if (req.body.type === "message" && req.body.message.type === "text") {
+//     const receivedText = event.message.text;
+//     const replyToken = event.replyToken;
+
+//     if (receivedText === "トイレ") {
+//       const message = {
+//         type: "text",
+//         text: "現在地を送信してください",
+//         quickReply: {
+//           items: [
+//             {
+//               type: "action",
+//               action: {
+//                 type: "location",
+//                 label: "現在地を送信",
+//               },
+//             },
+//           ],
+//         },
+//       };
+//       try {
+//         await client.replyMessage(replyToken, message);
+//       } catch (error) {
+//         console.error(error);
+//       }
+//       return;
+//     }
+
+//     const replyText = `あなたが送ったのは${receivedText}`;
+//     const message = {
+//       type: "text",
+//       text: replyText,
+//     };
+//     try {
+//       await client.replyMessage(replyToken, message);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }
+//   // 位置情報メッセージの処理
+//   else if (event.type === "message" && event.message.type === "location") {
+//     const { latitude, longitude, address } = event.message;
+//     const replyToken = event.replyToken;
+
+//     const message = {
+//       type: "text",
+//       text: `位置情報を受信しました！\n緯度: ${latitude}\n経度: ${longitude}\n住所: ${
+//         address || "不明"
+//       }`,
+//     };
+
+//     try {
+//       await client.replyMessage(replyToken, message);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   }
+// };
+
+// app.post("/webhook", line.middleware(config), (req, res) => {
+//   const events = req.body.events;
+//   Promise.all(events.map(handleEvent))
+//     .then(() => {
+//       res.json({ status: "ok" });
+//     })
+//     .catch((error) => {
+//       console.error(error);
+//       res.status(500).json({ status: "error" });
+//     });
+
+//   // ここで受信したイベント（メッセージなど）を処理するコードを後で追加
+// });
+
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => {
+//   console.log(`Server listening on port ${port}...`);
+// });
