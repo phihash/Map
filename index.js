b36afc6611a72e72502bc2f642e29ee3869f0a9e
phihash <insharp0220@gmail.com>
2026-04-16 01:21:27 +0900

    からハンドラ

--- index.js
+++ index.js
@@ -11,11 +11,47 @@ const config = {
 const client = new line.Client(config);
 const app = express();
 
+function handleList(event) {
+  // TODO: 一覧取得・返信
+  console.log("一覧", event.source.userId);
+}
+
+function handleAdd(event) {
+  // TODO: 登録フロー開始
+  console.log("追加", event.source.userId);
+}
+
+function handleDelete(event) {
+  // TODO: 削除フロー開始
+  console.log("削除", event.source.userId);
+}
+
+function handleEdit(event) {
+  // TODO: 編集フロー開始
+  console.log("編集", event.source.userId);
+}
+
+const commands = {
+  "追加": handleAdd,
+  "削除": handleDelete,
+  "編集": handleEdit,
+  "一覧": handleList,
+};
+
+
+
 app.post("/webhook", line.middleware(config), (req, res) => {
   // ここに色々かいていきます!
-  console.log(req.body);
+  const event = req.body.events[0];
+  const text = event.message.text;
+  const handler = commands[text];
+  if (handler) {
+    handler(event)
+  }  
+
   res.json({ status: "ok" });
 });
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
